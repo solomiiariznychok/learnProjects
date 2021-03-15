f7dd3b8872fd59d9423f5e75a29402ba25ca2604
solomiiariznychok <solomiyariznuchok@gmail.com>
2021-03-15 22:25:54 +0200

    remove folder anf=d commented lines

--- src/test/java/LoginSeleniumTest.java
+++ src/test/java/LoginSeleniumTest.java
@@ -11,9 +11,6 @@ public class LoginSeleniumTest  {
     public void login() { // TODO Auto-generated method stub
         System.setProperty("webdriver.chrome.driver", "D:\\browser_drivers\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        //111
-        //222
-        //333
         driver.manage().window().maximize();
         driver.get("https://www.linkedin.com/login");
         WebElement username = driver.findElement(By.id("username"));
