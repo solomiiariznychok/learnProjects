f0776a11ba9aaeaeff3b21370d6abebf583446fb
solomiiariznychok <riznychoksolomiia@gmail.com>
2021-10-10 23:39:25 +0300

    Update GoToEbayTest.java

--- src/test/java/GoToEbayTest.java
+++ src/test/java/GoToEbayTest.java
@@ -8,7 +8,7 @@ public class GoToEbayTest {
     @Test(priority = 2)
     public void goToEbay() {
        
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriznych\\Documents\\driver\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriznych\\Documents\\driver\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://signin.ebay.com/");
