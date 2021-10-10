b7981562077b2c7743fc79a8b2c83c049555a033
solomiiariznychok <riznychoksolomiia@gmail.com>
2021-10-10 23:34:40 +0300

    change driver path

--- src/test/java/GoToEbayTest.java
+++ src/test/java/GoToEbayTest.java
@@ -7,7 +7,8 @@ import java.util.concurrent.TimeUnit;
 public class GoToEbayTest {
     @Test(priority = 2)
     public void goToEbay() {
-        System.setProperty("webdriver.chrome.driver", "D:\\browser_drivers\\chromedriver\\chromedriver.exe");
+       
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriznych\\Documents\\driver\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://signin.ebay.com/");
