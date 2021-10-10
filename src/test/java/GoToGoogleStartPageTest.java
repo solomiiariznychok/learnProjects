7517c5c927de8f6692255a666c07594218cd650a
solomiiariznychok <riznychoksolomiia@gmail.com>
2021-10-10 23:39:44 +0300

    Update GoToGoogleStartPageTest.java

--- src/test/java/GoToGoogleStartPageTest.java
+++ src/test/java/GoToGoogleStartPageTest.java
@@ -9,7 +9,7 @@ public class GoToGoogleStartPageTest {
 
     @Test(priority = 1)
     public void goToGoogle() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriznych\\Documents\\driver\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sriznych\\Documents\\driver\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
